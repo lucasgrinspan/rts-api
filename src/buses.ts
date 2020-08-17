78b8b3749529e6d2a965e0914f8299ecc9c854ec
Lucas Grinspan <lucasgrinspan@gmail.com>
2020-08-17 12:37:11 -0400

    fix(apiCalls): requires agency parameter on certain calls

--- src/buses.ts
+++ src/buses.ts
@@ -8,35 +8,35 @@ export const getAgency = (agency = "") => {
 };
 
 // Get information about all of the available bus routes
-export const getRoutes = (agency = "") => {
+export const getRoutes = (agency: string) => {
     const url = generateUrl("routes", agency);
 
     return get(url, "routes");
 };
 
 // Get information about all of the segments that make the bus routes
-export const getSegments = (agency = "") => {
+export const getSegments = (agency: string) => {
     const url = generateUrl("segments", agency);
 
     return get(url, "segments");
 };
 
 // Get information about all of the available bus stops
-export const getStops = (agency = "") => {
+export const getStops = (agency: string) => {
     const url = generateUrl("stops", agency);
 
     return get(url, "bus stop");
 };
 
 // Get information about the buses currently on the routes
-export const getCurrentBuses = (agency = "") => {
+export const getCurrentBuses = (agency: string) => {
     const url = generateUrl("vehicle_statuses", agency, { include_arrivals: "true" });
 
     return get(url, "agency");
 };
 
 // Get the current announcements
-export const getAnnouncements = (agency = "") => {
+export const getAnnouncements = (agency: string) => {
     const url = generateUrl("announcements", agency);
 
     return get(url, "announcements");
